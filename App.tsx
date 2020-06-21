e695673584edd8f5243fdab48b72abf05f839562
Mandy Choi <hello@mandychoi.com.au>
2020-06-21 03:52:51 +0100

    refactor to use function component

--- App.tsx
+++ App.tsx
@@ -8,48 +8,57 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Text, Button, FlatList} from 'react-native';
 import repo from './components/Repository';
-import {DoughEvent} from './components/DoughEvent';
 
-class App extends React.Component {
-  doughEvents: Realm.Results<DoughEvent>;
-  constructor(props: Readonly<{}>) {
-    super(props);
-    this.doughEvents = repo.loadDoughEvents();
-  }
-
-  onButtonPress = () => {
-    repo.saveDoughEvent({name: 'Feed', creationDate: new Date()});
-    this.forceUpdate();
+export default function App() {
+  const getlastUpdateMessage = () => {
+    return doughEvents.length > 0
+      ? 'Last Update: ' + doughEvents[doughEvents.length - 1].creationDate
+      : '';
   };
 
-  render() {
-    const lastUpdateInfo =
-      this.doughEvents.length > 0
-        ? 'Last Update: ' +
-          this.doughEvents[this.doughEvents.length - 1].creationDate
-        : '';
-
+  const createEventButton = (eventName: string) => {
     return (
-      <View style={styles.container}>
-        <View style={styles.sectionDetails}>
-          <Text style={styles.label}>{lastUpdateInfo}</Text>
-          <View style={styles.button}>
-            <Button title="Feed" onPress={this.onButtonPress} />
-          </View>
-        </View>
-        <View style={styles.sectionList}>
-          <FlatList
-            data={this.doughEvents}
-            renderItem={({item}) => <Text>{item.name + "   " + item.creationDate}</Text>}
-            keyExtractor={(item, index) => '' + item.id}
-          />
-        </View>
-      </View>
+      <Button
+        title={eventName}
+        onPress={() => {
+          repo.saveDoughEvent({name: eventName, creationDate: new Date()});
+          setLastUpdateInfo(getlastUpdateMessage());
+        }}
+      />
     );
-  }
+  };
+  const doughEvents = repo.loadDoughEvents();
+  const [lastUpdateInfo, setLastUpdateInfo] = useState(getlastUpdateMessage());
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.sectionDetails}>
+        <Text style={styles.label}>{lastUpdateInfo}</Text>
+        <View style={styles.button}>{createEventButton('Feed')}</View>
+        <View style={styles.button}>{createEventButton('Mix')}</View>
+        <View style={styles.button}>{createEventButton('Bake')}</View>
+      </View>
+      <View style={styles.sectionList}>
+        <FlatList
+          data={doughEvents}
+          renderItem={({item}) => (
+            <Text>{item.name + '   ' + item.creationDate}</Text>
+          )}
+          keyExtractor={(item, index) => '' + item.id}
+        />
+      </View>
+      <Button
+        title="Clear"
+        onPress={() => {
+          repo.removeAll();
+          setLastUpdateInfo(getlastUpdateMessage());
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -59,10 +68,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
     height: 300,
   },
-  sectionList: {
-  },
-  sectionDetails: {
-  },
+  sectionList: {},
+  sectionDetails: {},
   label: {
     fontSize: 18,
   },
@@ -70,5 +77,3 @@ const styles = StyleSheet.create({
     backgroundColor: 'powderblue',
   },
 });
-
-export default App;
--- components/Repository.tsx
+++ components/Repository.tsx
@@ -5,7 +5,7 @@ import {DoughEvent} from './DoughEvent';
 
 const realm = new Realm({
   schema: [DoughEvent.schema],
-  deleteRealmIfMigrationNeeded: true
+  deleteRealmIfMigrationNeeded: true,
 });
 
 class Repository {
@@ -18,8 +18,23 @@ class Repository {
     const events = realm.objects('DoughEvent');
     const maxId = events.length > 0 ? events.sorted('id', true)[0].id : null;
     const nextId = maxId != null ? maxId + 1 : 0;
-    realm.create('DoughEvent', new DoughEvent(nextId, event.name, event.creationDate));
+    realm.create(
+      'DoughEvent',
+      new DoughEvent(nextId, event.name, event.creationDate),
+    );
     realm.commitTransaction();
+  }
+
+  removeAll() {
+    realm.write(() => {
+      realm.deleteAll();
+    });
+  }
+
+  close() {
+    if (realm !== null && !realm.isClosed) {
+      realm.close();
+    }
   }
 }
 
