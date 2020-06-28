2b2e3bb8013e4d038cd5904d19f4338949019ad2
Mandy Choi <hello@mandychoi.com.au>
2020-06-28 18:24:57 +0100

    #4 move to src folder and add alias

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {HomeScreen} from './components/HomeScreen';
-import {LoafRecordScreen} from './components/LoafRecordScreen';
+import {HomeScreen} from '@components/HomeScreen';
+import {LoafRecordScreen} from '@components/LoafRecordScreen';
 
 const Stack = createStackNavigator();
 
